7a537db6962a8705d458cbcd2cb0397253b0b48c
manivannancsmca <manivannancsmca@gmail.com>
2026-04-07 01:21:20 +0530

    content prevent

--- src/main/java/com/spring_ai_rag_vector/app/controller/ChatController.java
+++ src/main/java/com/spring_ai_rag_vector/app/controller/ChatController.java
@@ -3,6 +3,7 @@ package com.spring_ai_rag_vector.app.controller;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,12 +24,25 @@ public class ChatController {
 
     @PostMapping
     public String chat(@RequestBody String message) {
-    	System.out.println("message : " + message);
+
+        String strictSystemText = """
+                You are a strict technical assistant.
+                Use ONLY the provided CONTEXT to answer the QUESTION.
+                If the answer is not contained within the CONTEXT, say: 
+                "I'm sorry, but that information is not available in the uploaded documents."
+                Do not use any outside knowledge.
+                """;
+
         return ChatClient.builder(ollamaChatModel)
                 .build()
                 .prompt()
+                .system(strictSystemText)
                 .advisors(
                     QuestionAnswerAdvisor.builder(vectorStore)
+                        .searchRequest(SearchRequest.builder()
+                            .topK(3)
+                            .similarityThreshold(0.75)
+                            .build())
                         .build()
                 )
                 .user(message)
